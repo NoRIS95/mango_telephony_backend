d7a251790f2436fa186220049eeb402d25cc1e64
NoRIS95 <ryzhikov95@gmail.com>
2024-04-04 19:30:15 +0300

    Methods are implemented and documentation is drawn up

--- main.py
+++ main.py
@@ -12,55 +12,56 @@ api = MangoAPI(api_vpbx_url, api_vpbx_key, api_vpbx_salt)
 app = FastAPI()
 
 @app.post("/send_sms/")
-def send_sms(number:str, text: str, from_ext: str, command_id="base",  sender=None):
+def send_sms(number:str, text: str, from_ext: str, command_id="base", sender=None):
     return api.sms(number=number, text=text, from_ext=from_ext, command_id=command_id, sender=sender)
 
 @app.post("/callback/")
-def make_callback(command_id:str, from_ext:str, from_num:str, to_num:str, line=None, sip_head=None):
-    return api.callback(from_ext=from_ext, to_num=to_num, line=line)
+def make_callback(from_ext: str, to_num: str, command_id="base", from_num=None, line=None, sip_head=None):
+    return api.callback(from_ext=from_ext,\
+     to_num=to_num, command_id=command_id, from_num=from_num, line=line, sip_head=sip_head)
 
 @app.post("/group_callback/")
-def make_group_callback(from_ext: str, to_num: str, line: str):
-    return group_callback(from_ext=from_ext, to_num=to_num, line=line)
+def make_group_callback(from_ext: str, to_num: str, command_id="base", line=None):
+    return group_callback(from_ext=from_ext, to_num=to_num, command_id=command_id, line=line)
 
 @app.post("/end_call/")
-def end_call(call_id: str,):
-    return api.hangup(call_id=call_id)
+def end_call(call_id: str, command_id="base"):
+    return api.hangup(command_id=command_id, call_id=call_id)
 
 @app.post("/start_record/")
-def start_record(call_id: str, call_party_number: str):
-    return api.start_record(call_id=call_id,
+def start_record(call_id: str, call_party_number: str, command_id="base"):
+    return api.start_record(command_id=command_id, call_id=call_id,
                  call_party_number=call_party_number)
 
 @app.post("/start_play/")
-def start_play(call_id: str, internal_id: str):
+def start_play(call_id: str, internal_id: str, command_id="base", after_play_time=None):
     return api.start_play(call_id=call_id,
-               internal_id=internal_id)
+               internal_id=internal_id, command_id=command_id, after_play_time=after_play_time)
 
 @app.post("/route_call/")
-def route_call(call_id: str, to_number: str):
-    return api.route(call_id=call_id,
-          to_number=to_number)
+def route_call(call_id: str, to_number: str, command_id="base", sip_headers=None):
+    return api.route(command_id=command_id, call_id=call_id,
+          to_number=to_number, sip_headers=sip_headers)
 
 @app.post("/transfer_call/")
-def transfer_call(call_id: str, to_number: str, method: str, initiator: str):
+def transfer_call(call_id: str, to_number: str, method: str, initiator: str, command_id=None):
     return api.transfer(call_id=call_id, to_number=to_number, method=method,
-             initiator=initiator)
+             initiator=initiator, command_id=command_id)
 
 @app.post("/get_stats_to/")
-def get_stats_to(to_ext: str, date_from: int, date_to: int, fields: str):
+def get_stats_to(to_ext: str, date_from: int, date_to: int, fields: str, to_num=None, request_id=None):
     return api.get_stats_to(to_ext=to_ext, date_from=date_from, date_to=date_to,
-                 fields=fields)
+                 fields=fields, to_num=to_num, request_id=request_id)
 
 @app.post("/get_stats_from/")
-def get_stats_from(from_ext: str, date_from: int, date_to: int, fields: str):
+def get_stats_from(from_ext: str, date_from: int, date_to: int, fields: str, from_num=None, request_id=None):
     return api.get_stats_from(from_ext=from_ext, date_from=date_from, date_to=date_to,
-                 fields=fields)
+                 fields=fields, from_num=from_num, request_id=request_id)
 
 @app.post("/get_stats_call_party/")
-def get_stats_call_party(call_party_ext: str, date_from: int, date_to: int, fields: str):
+def get_stats_call_party(call_party_ext: str, date_from: int, date_to: int, fields: str, call_party_num=None, request_id=None):
     return api.get_stats_call_party(call_party_ext=call_party_ext, date_from=date_from, date_to=date_to,
-                         fields=fields)
+                         fields=fields, call_party_num=call_party_num, request_id=request_id)
 
 @app.post("/get_dct_user_info/")
 def get_dct_user_info(number: str):
@@ -71,19 +72,20 @@ def get_dct_user_history(number: str):
     return api.dct_user_history(number=number)
 
 
-@app.post("/user_list")
-def get_user_list():
+@app.post("/user_list") #РАБОТАЕТ
+def get_user_list(ext_fields=None, extension=None):
     fields_list = ["general.user_id", "general.sips", "groups", "general.access_role_id", "telephony.dial_alg",
                "telephony.numbers.schedule", "telephony.line_id", "telephony.trunk_number_id", "general.mobile",
                "general.login", "general.use_status", "general.use_cc_numbers"]
-    return api.user_list(ext_fields=fields_list)
+    return api.user_list(ext_fields=fields_list, extension=extension)
 
 @app.post("/group_list")
-def get_group_list(group_id: str, operator_id: str):
-    return api.group_list(group_id=group_id, operator_id=operator_id)
+def get_group_list(group_id=None, operator_id=None, operator_extension=None, show_users=1):
+    return api.group_list(group_id=group_id, operator_id=operator_id,\
+        operator_extension=operator_extension, show_users=show_users)
 
 @app.post("/balance")
-def get_balance()
+def get_balance():
     return api.balance()
 
 @app.post("/lines")
@@ -95,11 +97,11 @@ def get_audio():
     return api.audio()
 
 @app.post("/set_schema")
-def set_schema(schema: int, line: int):
-	return api.set_schema(schema=schema, line=line)
+def set_schema(schema: int, line: int, trunk=None):
+	return api.set_schema(schema=schema, line=line, trunk=trunk)
 
 @app.post("/get_ext_field")
-def get_ext_field(ext_fields: int):
+def get_ext_field(ext_fields=None):
 	return api.schemas(ext_fields=ext_fields)
 
 @app.post("/roles")
@@ -114,7 +116,7 @@ def get_sips_list():
 def get_domains_list():
     return api.domains_list()
 
-@app.post("/trunk_num_list")
+@app.post("/trunk_num_list") 
 def get_trunk_num_list():
     return api.trunk_num_list()
 
@@ -127,12 +129,12 @@ def get_bwlist_nums():
     return api.bwlist_nums()
 
 @app.post("/add_bwlist")
-def add_bwlist(number: str):
-    return api.bwlist_add(number=number)
+def add_bwlist(number: str, list_type='white', num_type='tel', comment='API'):
+    return api.bwlist_add(number=number, list_type=list_type, num_type=num_type, comment=comment)
 
 @app.delete("/delete_bwlist")
-def delete_bwlist(number: int):
-    return api.bwlist_del(number)
+def delete_bwlist(num_id: int):
+    return api.bwlist_del(num_id=num_id)
 
 
 @app.post("/campaign_info")
@@ -140,15 +142,19 @@ def get_campaign_info(campaign_id: int):
     return api.campaign_info(campaign_id=campaign_id)
 
 @app.post("/ab_contact_init")
-def make_ab_contact_init(query: str, limit_rows: int):
-    return api.ab_contact_init(query=query, limit_rows=limit_rows)
+def make_ab_contact_init(query: str, limit_rows: int, order=None, contact_ext_fields=None):
+    return api.ab_contact_init(query=query, limit_rows=limit_rows, order=order, contact_ext_fields=contact_ext_fields)
 
 
-@app.post("/cc_deal_list")
+@app.post("/cc_deal_list") 
 def get_cc_deal_list(product_id: int, status: str, from_date=None, until_date=None,
                      members_ids=None, contact_id=None):   
-	return api.cc_deal_list(product_id=product_id)
+	return api.cc_deal_list(product_id=product_id, status=status, from_date=from_date,
+        until_date=until_date, members_ids=members_ids, contact_id=contact_id)
 
 @app.post("/cc_deal_funnels_list")
 def get_cc_deal_funnels_list(product_id: int):   
     return api.cc_deal_funnels_list(product_id=product_id)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
